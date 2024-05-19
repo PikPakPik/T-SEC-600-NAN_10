fcb31771a94142b239452f12824f3972431d54d7
Alexandre.T <alexandre.tressel@icloud.com>
2024-05-19 11:52:44 +0200

    update tree and script to see the subject of boxes

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 Ce README contient des liens cliquables pour accéder aux fichiers du dossier `log$`.
 
-## Fichiers
+## Boxes
 
-- [SECRET_OF_THE_MAW](writeups/SECRET_OF_THE_MAW.md)
-- [TOSS_A_COIN](writeups/TOSS_A_COIN.md)
-- [YER_A_WIZARD](writeups/YER_A_WIZARD.md)
+###  Basic - Privilege escalation
+
+- [SECRET_OF_THE_MAW](writeups/1%20-%20Basic%20-%20Privilege%20escalation/SECRET_OF_THE_MAW.md)
+- [TOSS_A_COIN](writeups/1%20-%20Basic%20-%20Privilege%20escalation/TOSS_A_COIN.md)
+- [YER_A_WIZARD](writeups/1%20-%20Basic%20-%20Privilege%20escalation/YER_A_WIZARD.md)
+
+###  Medium - CVE and SSH Tunneling
+
+
+###  Advanced - Realistic boxes
+
+
 
 N'hésitez pas à explorer ces fichiers pour résoudre le CTF !
--- update-readme.sh
+++ update-readme.sh
@@ -4,16 +4,25 @@ echo "# CTF README" > README.md
 echo "" >> README.md
 echo "Ce README contient des liens cliquables pour accéder aux fichiers du dossier \`log$\`." >> README.md
 echo "" >> README.md
-echo "## Fichiers" >> README.md
+echo "## Boxes" >> README.md
 echo "" >> README.md
 
-for file in writeups/*; do
-    if [ -d "$file" ]; then
-        continue
-    fi
-    filename=$(basename -- "$file")
-    filename="${filename%.*}"
-    echo "- [$filename](writeups/$filename.md)" >> README.md
+for dir in writeups/*; do
+    echo "### $(echo $dir | cut -d'-' -f2-)" >> README.md
+    echo "" >> README.md
+
+    for file in "$dir"/*.md; do
+        if [ ! -f "$file" ]; then
+            echo "No files found in $dir"
+            continue
+        fi
+        filename=$(basename -- "$file")
+        filename="${filename%.*}"
+        link="${dir// /%20}/$filename.md"
+        echo "- [$filename]($link)" >> README.md
+    done
+
+    echo "" >> README.md
 done
 
 echo "" >> README.md
